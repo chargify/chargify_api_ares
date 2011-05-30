ef4220cc3e4df7c7f4ae8e85c453b3be51c2360f
Larry Sprock <larry@lucidbleu.com>
2011-05-30 15:11:33 -0700

    Cleanup spec_helpers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,6 @@
+# encoding: utf-8
 require 'rubygems'
-require 'spec'
-$LOAD_PATH.unshift(File.dirname(__FILE__))
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+require 'bundler/setup'
 
 require 'chargify_api_ares'
 
