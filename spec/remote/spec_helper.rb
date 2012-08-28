bb39f0d042003ee2b196672c0a41e1c856846f21
Ryan Schlesinger <ryan@instanceinc.com>
2012-08-28 07:00:59 -0700

    Update remote_spec metadata handling

--- spec/remote/spec_helper.rb
+++ spec/remote/spec_helper.rb
@@ -13,6 +13,7 @@ end
 RSpec.configure do |config|
   config.filter_run :focused => true
   config.run_all_when_everything_filtered = true
+  config.treat_symbols_as_metadata_keys_with_true_values = true
   config.alias_example_to :fit, :focused => true
   config.color_enabled = true
   config.before(:all) do
