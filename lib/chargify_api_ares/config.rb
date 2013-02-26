ebefa43b9c7aa37b710842fa0183e33417884daa
Jeremy W. Rowe <jeremy.w.rowe@gmail.com>
2013-02-26 11:54:48 -0600

    updated documentation, added regression spec for improper cc on subscription, and updated configuration api

--- lib/chargify_api_ares/config.rb
+++ lib/chargify_api_ares/config.rb
@@ -1,18 +1,18 @@
 module Chargify
   class << self
-    attr_accessor :subdomain, :api_key, :site, :format, :timeout
+    attr_accessor :subdomain, :api_key, :site, :format, :timeout, :domain, :protocol
 
     def configure
       yield self
-
+      self.protocol  = protocol  || "https"
+      self.domain    = domain    || "chargify.com"
+      self.format    = format    || :xml
+      self.subdomain = subdomain || "test"
       Base.user      = api_key
       Base.password  = 'X'
       Base.timeout   = timeout unless (timeout.blank?)
-      
-      self.site ||= "https://#{subdomain}.chargify.com"
-      Base.site = site
-      self.format ||= :xml
-      Base.format = format
+      Base.site      = site || "#{protocol}://#{subdomain}.#{domain}"
+      Base.format    = format
     end
   end
 end
