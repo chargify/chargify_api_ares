c4fe9a8e7bb0232c09129586e85b17a295a0140f
Jeremy W. Rowe <jeremy.w.rowe@gmail.com>
2014-08-17 12:30:47 -0400

    restore 1.8.7 style hash syntax

--- lib/chargify_api_ares/resources/invoice.rb
+++ lib/chargify_api_ares/resources/invoice.rb
@@ -2,19 +2,19 @@ module Chargify
   class Invoice < Base
 
     def self.find_by_invoice_id(id)
-      find(:first, params: {id: id})
+      find(:first, {:params => {:id => id}})
     end
 
     def self.find_by_subscription_id(id)
-      find(:all, params: {subscription_id: id})
+      find(:all, {:params => {:subscription_id => id}})
     end
 
     def self.unpaid_from_subscription(subscription_id)
-      find(:all, params: {subscription_id: subscription_id, state: "unpaid"})
+      find(:all, {:params => {:subscription_id => subscription_id, :state => "unpaid"}})
     end
 
     def self.unpaid
-      find(:all, params: {state: "unpaid"})
+      find(:all, {:params => {:state => "unpaid"}})
     end
   end
 end
