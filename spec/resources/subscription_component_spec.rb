ebd3d35369b28ec357413521af87500a1b3b9fec
Jeremy W. Rowe <jeremy.w.rowe@gmail.com>
2014-12-13 11:18:49 -0500

    update specs to match api for ActiveResource::Collection in active resource 4.*

--- spec/resources/subscription_component_spec.rb
+++ spec/resources/subscription_component_spec.rb
@@ -28,27 +28,33 @@ describe Chargify::Subscription::Component, :fake_resource do
       FakeWeb.register_uri(:get, "#{test_domain}/subscriptions/#{@subscription.id}.xml", :body => @subscription.to_xml)
       FakeWeb.register_uri(:get, "#{test_domain}/subscriptions/#{@subscription.id}/components.xml", :body => @subscriptions_components.to_xml(:root => 'components'))
     end
-    
+
     it "returns an array of components from Chargify::Subscription::Component.find(:all, :params => {:subscription_id => @subscription.id})" do
-      Chargify::Subscription::Component.find(:all, :params => {:subscription_id => @subscription.id}).should == @subscriptions_components
+      expected = @subscriptions_components
+      actual   = Chargify::Subscription::Component.find(:all, :params => {:subscription_id => @subscription.id})
+      actual   = actual.elements if actual.respond_to?(:elements)
+      expect(actual).to eql(expected)
     end
 
     it "returns an array of components from Chargify::Subscription.find(2).components" do
       subscription = Chargify::Subscription.find(@subscription.id)
-      subscription.components.should == @subscriptions_components
+      expected     = @subscriptions_components
+      actual       = subscription.components
+      actual       = actual.elements if actual.respond_to?(:elements)
+      expect(actual).to eql(expected)
     end
   end
-  
+
   describe "reading a subscription component" do
     before(:each) do
       FakeWeb.register_uri(:get, "#{test_domain}/subscriptions/#{@subscription.id}.xml", :body => @subscription.to_xml)
       FakeWeb.register_uri(:get, "#{test_domain}/subscriptions/#{@subscription.id}/components/#{@sc1.component_id}.xml", :body => @sc1.to_xml)
     end
-    
+
     it "returns the subscription's component resource from Chargify::Subscription::Component.find(1, :params => {:subscription_id => 1})" do
       Chargify::Subscription::Component.find(@sc1.component_id, :params => {:subscription_id => @subscription.id}).should == @sc1
     end
-    
+
     it "returns the subscription's component resource from Chargify::Subscription.find(1).component(1)" do
       subscription = Chargify::Subscription.find(@subscription.id)
       subscription.component(@sc1.component_id).should == @sc1
@@ -58,7 +64,7 @@ describe Chargify::Subscription::Component, :fake_resource do
   describe "updating a subscription component" do
     before(:each) do
       @new_allocated_quantity = @sc1.allocated_quantity + 5
-      
+
       FakeWeb.register_uri(:get, "#{test_domain}/subscriptions/#{@subscription.id}.xml", :body => @subscription.to_xml)
       FakeWeb.register_uri(:get, "#{test_domain}/subscriptions/#{@subscription.id}/components/#{@sc1.component_id}.xml", :body => @sc1.to_xml)
 
@@ -68,14 +74,14 @@ describe Chargify::Subscription::Component, :fake_resource do
       FakeWeb.register_uri(:put, "#{test_domain}/subscriptions/#{@subscription.id}/components/#{@sc1.component_id}.xml", :body => @sc1_prime.to_xml)
       FakeWeb.register_uri(:get, "#{test_domain}/subscriptions/#{@subscription.id}/components/#{@sc1.component_id}.xml", :body => @sc1_prime.to_xml)
     end
-    
+
     it "updates the subscription's component allocated quantity" do
       component = Chargify::Subscription::Component.find(@sc1.component_id, :params => {:subscription_id => @subscription.id})
       component.allocated_quantity = @new_allocated_quantity
 
       result = component.save
       result.should be_true
-      
+
       Chargify::Subscription::Component.find(@sc1.component_id, :params => {:subscription_id => @subscription.id}).should == @sc1_prime
     end
   end
