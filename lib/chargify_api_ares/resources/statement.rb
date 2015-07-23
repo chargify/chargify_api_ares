b23602a4f94ae8bc42ca3e5ef9da42a701c3f863
hoblin <evgeny.gurin@gmail.com>
2015-07-23 23:56:07 +0300

    Chargify::Statement.find_pdf method

--- lib/chargify_api_ares/resources/statement.rb
+++ lib/chargify_api_ares/resources/statement.rb
@@ -1,4 +1,9 @@
 module Chargify
   class Statement < Base
+    def self.find_pdf(scope, options = {})
+      prefix_options, query_options = split_options(options[:params])
+      path = element_path(scope, prefix_options, query_options).gsub(/\.\w+$/, ".pdf")
+      connection.get(path, headers).body
+    end
   end
 end
