a43c128a88db463a3c908afe3a166e131a7d5133
Ryan Schlesinger <ryan@outstand.com>
2015-12-22 13:43:17 -0800

    Add Chargify::Allocation.bulk_create

--- lib/chargify_api_ares/resources/allocation.rb
+++ lib/chargify_api_ares/resources/allocation.rb
@@ -1,9 +1,26 @@
 module Chargify
-  
   class Allocation < Base
-    
-    self.prefix = "/subscriptions/:subscription_id/components/:component_id/" 
-    
+    self.prefix = "/subscriptions/:subscription_id/components/:component_id/"
+
+    def self.bulk_create_prefix(opts = {})
+      subscription_id = opts[:subscription_id]
+      raise ArgumentError, 'subscription_id required' if subscription_id.nil?
+
+      "/subscriptions/#{subscription_id}/allocations.#{format.extension}"
+    end
+
+    def self.bulk_create(opts = {})
+      return [] if opts[:allocations].blank?
+
+      subscription_id = opts.delete(:subscription_id)
+      raise ArgumentError, 'subscription_id required' if subscription_id.nil?
+
+      response = connection.post(
+        bulk_create_prefix(subscription_id: subscription_id),
+        format.encode(opts),
+        headers
+      )
+      instantiate_collection(format.decode(response.body))
+    end
   end
-  
 end
