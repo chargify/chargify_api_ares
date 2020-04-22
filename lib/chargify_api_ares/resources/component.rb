b07f90789f1829986e5c55f9737e70af2127c544
Jaime Giraldo <sposmen@gmail.com>
2020-04-22 11:18:08 -0500

    Adding PricePoints to Components

--- lib/chargify_api_ares/resources/component.rb
+++ lib/chargify_api_ares/resources/component.rb
@@ -1,4 +1,12 @@
 module Chargify
   class Component < Base
+    class PricePoint < Base
+      self.prefix = "/components/:component_id/"
+    end
+
+    def price_points(params = {})
+      params.merge!(:component_id => self.id)
+      ::Chargify::Component::PricePoint.find(:all, :params => params)
+    end
   end
 end
