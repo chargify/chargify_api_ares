92aff9eb48672cd5406a9cac300c7ced4471e60c
Matias Nieves <matinieves@gmail.com>
2023-11-09 17:21:42 -0300

    Include invoice payment typeon create payment request.

--- lib/chargify_api_ares/resources/invoice_payment.rb
+++ lib/chargify_api_ares/resources/invoice_payment.rb
@@ -3,8 +3,8 @@ module Chargify
     self.prefix = '/invoices/:invoice_id/'
     self.collection_name = 'payments'
 
-    def self.create(invoice_id, payment_attrs)
-      super(invoice_id:, payment: payment_attrs)
+    def self.create(invoice_id, payment_attrs, type = 'payment')
+      super(invoice_id:, payment: payment_attrs, type:)
     end
   end
 end
